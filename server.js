96cd063c5e727e4ace3f3a64f707215dd08c0fbc
Vinaykrish25 <vinaykrish2002@gmail.com>
2025-07-19 22:15:45 +0530

    Updated server.js

--- server.js
+++ server.js
@@ -37,5 +37,4 @@ app.use('/employee-leave', employeeLeaveRoutes);
 app.use('/employee-dashboard', employeeDashboardRoutes);
 app.use('/employee-profile', employeeProfileRoutes);
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+module.exports = app;
